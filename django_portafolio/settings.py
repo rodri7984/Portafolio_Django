f30028eb8a6e6975efa1b9fa17779cfce1c044b8
rodri <rodri@gmail.com>
2024-10-15 18:32:07 -0300

    test debug

--- django_portafolio/settings.py
+++ django_portafolio/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b(05!2-na6tk=qto=deva%%(id)2sf1d2_2u%t5)p5@y(msjty'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['https://portafoliodjango-production.up.railway.app','127.0.0.1', 'localhost']
 
